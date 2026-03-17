52c0596c5042ae46ecf2ea7aa71c4d9af15c9f3c
arthuzzz <arthur.santos@alu.ufc.br>
2026-03-17 18:22:20 +0000

    save2

---
+++ fup/sobrou/go/draft.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"go/scanner"
+)
+func main() {
+    var quan1, quan2, quan3 int
+    var prod1, prod2, prod3 float64
+    var money float64
+
+    fmt.Scan(&quan1)
+    fmt.Scan(&quan2)
+    fmt.Scan(&quan3)
+
+    fmt.Scan(&prod1)
+    fmt.Scan(&prod2)
+    fmt.Scan(&prod3)
+
+}
