180d8857d22e702777e92be6a74f6239e2daf073
arthuzzz <arthur.santos@alu.ufc.br>
2026-03-23 16:52:14 +0000

    save3

--- fup/sobrou/go/draft.go
+++ fup/sobrou/go/draft.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
-	"go/scanner"
 )
+
 func main() {
     var quan1, quan2, quan3 int
     var prod1, prod2, prod3 float64
     var money float64
 
-    fmt.Scan(&quan1)
-    fmt.Scan(&quan2)
-    fmt.Scan(&quan3)
+    fmt.Scan(&quan1, &quan2, &quan3)
+    fmt.Scan(&prod1, &prod2, &prod3)
+    fmt.Scan(&money)
 
-    fmt.Scan(&prod1)
-    fmt.Scan(&prod2)
-    fmt.Scan(&prod3)
+    Gasto := float64(quan1)*prod1 + float64(quan2)*prod2 + float64(quan3)*prod3
 
+    Troco := money - Gasto
+
+    fmt.Printf("%.2f\n", Troco)
 }
---
+++ sandbox/00_rascunhos/Lâmpadas e Portas/draft.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main() {
+    Status := true
+
+    if Status == false{
+    fmt.Println("A lâmpada está apagada")
+}    else if Status == true{
+    fmt.Println("A lâmpada está acesa")}
+
+fmt.Println("e")
+
+Statusd := false
+
+if Statusd == false{
+    fmt.Println("a porta está fechada")
+} else if Statusd == true{
+    fmt.Println("a porta está aberta")
+}
+}
