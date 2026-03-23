40186ed2a67e0b3485f84cc1e7a3fc2a2a8648b6
arthuzzz <arthur.santos@alu.ufc.br>
2026-03-23 18:26:55 +0000

    save3.2

---
+++ fup/bala/go/draft.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+import "math"
+func main() {
+    var x1, y1 float64
+    var x2, y2 float64
+    fmt.Scan(&x1, &y1)
+    fmt.Scan(&x2, &y2)
+    
+    distancia := math.Sqrt(math.Pow((x2-x1), 2) + math.Pow((y2-y1), 2))
+
+    fmt.Printf("%.2f\n", distancia)
+}
