ee864cd7f5c1c94a249fc061bce171909300e3de
arthuzzz <arthur.santos@alu.ufc.br>
2026-05-05 18:04:22 +0000

    save10.2.1

---
+++ fup/esconde/src/go/draft.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+func main() {
+    var N int
+    fmt.Scan(&N)
+
+    for i:= 1; i <= N; i+=2 {
+            fmt.Println(i)
+
+        }
+    for i:= N; i >= 0; i-- {
+        if i % 2 == 0 {
+        fmt.Println(i)
+        }
+    }
+        
+    }
+
