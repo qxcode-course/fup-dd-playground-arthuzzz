50d1aae4d0dea131758c6e3f2cdf869408e38932
arthuzzz <arthur.santos@alu.ufc.br>
2026-05-06 13:05:58 +0000

    save11

--- eval1/ceu/src/go/draft.go
+++ eval1/ceu/src/go/draft.go
@@ -7,16 +7,14 @@ func main() {
     fmt.Print("[ ")
 
     for i:= 0; i <= 10; i++ {
-        
         if i == N {
             continue
         }
-        if i == 10 {
-            fmt.Print( "ceu ")
+        if i==10 {
+            fmt.Print("ceu ")
             continue
         }
-        
         fmt.Print(i, " ")
-    }
+    } 
     fmt.Print("]\n")
 }
